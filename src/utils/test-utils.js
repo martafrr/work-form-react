6cfe07f41c1d03aa087bdcf8ac3b66863b8db36d
Marta Farre <martafarreribas@gmail.com>
2020-02-28 16:03:09 +0100

    Test utils file

---
+++ src/utils/test-utils.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from '../store/reducers/rootReducer';
+import { middlewares } from '../store';
+
+export const findByTestAtr = (component, atr) => {
+    const wrapper = component.find(`[data-test='${atr}']`);
+    return wrapper;
+};
+
+export const testStore = (initialState) => {
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
+};
